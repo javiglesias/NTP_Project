35666a8c368d1870ce84a306143ea2c97a1af1f8
Javier Iglesias <javiglesias@outlook.com>
2016-04-04 21:41:49 +0200

    Agregado Database Errores
    -He aNadido un excel con los codigos de error y su significado.
    -Algunas modificaciones en el sever.java.

--- Projecto_NTP/src/code/Server.java
+++ Projecto_NTP/src/code/Server.java
@@ -1,33 +1,37 @@
 package code;
+import java.util.Random;
+import java.util.Scanner;
+
 import javax.ws.rs.Path;
 
 //SERVIDOR NO NTP
-import org.eclipse.jdt.internal.compiler.parser.Scanner;
 @Path("/no_ntp")
 public class Server extends Thread{
 	Thread th0 = new Server();
-	Scanner sc = new Scanner();
+	Scanner sc;
 	public Server()
 	{
+		sc = new Scanner(System.in);
 		System.out.println("<Iniciando un servidor NO-NTP>");
+		System.out.println("Nombre de la maquina?: ");
+		th0.setName(sc.nextLine());
 		th0.start();
 	}
 	@Path("/get_time")
-	public long[] get_time()
-	{
-		long[] time = new long[2];
-		time[0] = System.currentTimeMillis();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		time[1] = System.currentTimeMillis();
-		return time;
-	}
 	public void run()
 	{
-	
+		long[] time = new long[2];
+		Random rng = new Random();
+		System.out.println(this.getName()+" arrancada.");
+		System.out.println("Pulse una tecla para continuar.");
+		sc.nextLine();
+		time[0] = System.currentTimeMillis();
+		try {
+			Thread.sleep(rng.nextInt(10-1)+1);
+		} catch (InterruptedException e) {
+			System.out.println("ERROR 1");
+		}
+		time[1] = System.currentTimeMillis();
+		//LLAMAMOS AL SERVIDOR NTP PASANDOLE LOS TIEMPOS COMO PARAMETRO
 	}
 }
