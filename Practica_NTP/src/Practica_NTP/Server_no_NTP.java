beb30accfac44bb09d97b859855e2771347a3f8d
Javier Iglesias <javiglesias@outlook.com>
2016-04-07 01:00:44 +0200

    Almost done!
    Now i know how to continue....

--- Practica_NTP/src/Practica_NTP/Server_no_NTP.java
+++ Practica_NTP/src/Practica_NTP/Server_no_NTP.java
@@ -1,30 +1,33 @@
 package Practica_NTP;
+import java.net.URI;
 //Java 1.8 tomcat 8 API 2.5
 //Ruta de acceso: http://localhost:8080/Practica_NTP/main/get_time
 import java.util.Random;
 import java.util.Scanner;
-
+import javax.ws.rs.client.Client;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 
 @Path("/main")
-public class Server_no_NTP extends Thread{
-	@GET
-	@Path("/mostrar")
-	public String mostrar()
-	{
-		return("mostra");
-	}
+public class Server_no_NTP extends Thread
+{
+	
+	
 	@GET
 	@Path("/get_time")
-	public void get_time()
+	public double[] get_time()
 	{
 		double []time = new double[2];
 		Random rng = new Random();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Maquina arrancada.");
+		//System.out.println("Introduzca la IP y PUERTO del servidor NTP(IP:PUERTO): ");
+		//IP = sc.nextLine();
 		System.out.println("Pulse una tecla para continuar.");
 		sc.nextLine();
 		time[0] = System.currentTimeMillis();//INICIALIZAMOS EL TIME
@@ -36,5 +39,6 @@ public class Server_no_NTP extends Thread{
 		time[1] = System.currentTimeMillis();//TERMINAMOS LA MEDICION DE TIEMPO
 		//LLAMAMOS AL SERVIDOR NTP PASANDOLE LOS TIEMPOS COMO PARAMETRO
 		System.out.println(time[1]-time[0]+" Ms");
+		return time;
 	}
 }
--- src/Server_Practica_NTP/ServerNTP.java
+++ src/Server_Practica_NTP/ServerNTP.java
@@ -1,33 +1,39 @@
 package Server_Practica_NTP;
 
+import java.net.URI;
 import java.util.Scanner;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.UriBuilder;
 
 //Java 1.8 tomcat 8 API 2.5
 //Ruta de acceso: http://localhost:8080/Server_Practica_NTP/server/pedir
 @Path("/server")
 public class ServerNTP {
-	@GET
-	@Path("/mostrar")
-	public String mostrar()
+	static String ip1="",ip2="";
+	//static String IP = "";
+	/*@GET
+	@Path("/cargar")
+	public void cargar(String IP)
 	{
-		/*
-		 * Client cliente = ClientBuilder.newClient();
-		URI url1 = UriBuilder.fromUri("http://"+ip1+"/no_ntp");
-		URI url2 = UriBuilder.fromUri("http://"+ip2+"/no_ntp");
-		 * */
-		return("mostra");
-	}
+		WebTarget target;
+		Client cliente = ClientBuilder.newClient();
+		URI url = UriBuilder.fromUri("http://"+IP+"/Practica_NTP/main").build();
+		target = cliente.target(url);
+		
+	}*/
 	@GET
 	@Path("/pedir")
 	public void pedir()
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("introduzca la ip de pc1 y puerto:");
-		String ip1 = sc.nextLine();
+		ip1 = sc.nextLine();
 		System.out.println("introduzca la ip de pc2 y puerto:");
-		String ip2 = sc.nextLine();
+		ip2 = sc.nextLine();
 	}
 }
